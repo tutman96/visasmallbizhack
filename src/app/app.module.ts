942eb2514018e63fca2a5c933ac2f16e3a79718c
Leo Martinez <l3omtz@gmail.com>
2017-12-02 11:51:27 -0500

    base

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,40 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
+import { LandingComponent } from './landing/landing.component';
+import { CommandCenterComponent } from './command-center/command-center.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const appRoutes: Routes = [
+  {
+    path: 'crisis-center',
+    component: CommandCenterComponent },
+  {
+    path: 'welcome',
+    component: LandingComponent
+  },
+  { path: '',
+    redirectTo: '/welcome',
+    pathMatch: 'full'
+  },
+  { path: '**', component: PageNotFoundComponent }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingComponent,
+    CommandCenterComponent,
+    PageNotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      { enableTracing: true }
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/command-center/command-center.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-command-center',
+  templateUrl: './command-center.component.html',
+  styleUrls: ['./command-center.component.css']
+})
+export class CommandCenterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
